851ea9db426937399af3e17ba6fb5d2da35b1253
chisoftltd <chisoftltd@gmail.com>
2021-03-02 16:51:24 +0000

    NumPy Splitting Array

--- PythonNumPy.py
+++ PythonNumPy.py
@@ -275,3 +275,45 @@ print()
 arr = np.dstack((arr1, arr2))
 print(arr)
 print()
+
+# NumPy Splitting Array
+# Splitting NumPy Arrays
+newarr = np.array_split(arr, 3)
+print(newarr)
+print()
+
+arr = np.array([1, 2, 3, 4, 5, 6])
+newarr = np.array_split(arr, 5)
+print(newarr)
+print()
+
+# Split Into Arrays
+newarr = np.array_split(arr, 3)
+print(newarr[0])
+print(newarr[1])
+print(newarr[2])
+print()
+
+# Splitting 2-D Arrays
+arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
+newarr = np.array_split(arr, 3)
+print(newarr)
+print()
+
+# Split the 2-D array into three 2-D arrays 
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+newarr = np.array_split(arr, 3)
+print(newarr)
+print()
+
+# The example below also returns three 2-D arrays, but they are split along the row (axis=1)
+newarr = np.array_split(arr, 3, axis=1)
+print(newarr)
+print()
+
+# Use the hsplit() method to split the 2-D array into three 2-D arrays along rows
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+
+newarr = np.hsplit(arr, 3)
+
+print(newarr)
