ef1d3f103d1571f9a4194a0f2985810aace07170
chisoftltd <chisoftltd@gmail.com>
2021-03-02 15:31:52 +0000

    NumPy Joining Array

--- PythonNumPy.py
+++ PythonNumPy.py
@@ -238,3 +238,40 @@ arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
 for idx, x in np.ndenumerate(arr):
   print(idx, x)
 print()
+
+# NumPy Joining Array
+# Joining NumPy Arrays
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.concatenate((arr1, arr2))
+print(arr)
+print()
+
+# Join two 2-D arrays along rows (axis=1)
+arr1 = np.array([[1, 2], [3, 4]])
+arr2 = np.array([[5, 6], [7, 8]])
+arr = np.concatenate((arr1, arr2), axis=1)
+print(arr)
+print()
+
+# Joining Arrays Using Stack Functions
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.stack((arr1, arr2), axis=1)
+print(arr)
+print()
+
+# Stacking Along Rows
+arr = np.hstack((arr1, arr2))
+print(arr)
+print()
+
+# Stacking Along Columns
+arr = np.vstack((arr1, arr2))
+print(arr)
+print()
+
+# Stacking Along Height (depth)
+arr = np.dstack((arr1, arr2))
+print(arr)
+print()
