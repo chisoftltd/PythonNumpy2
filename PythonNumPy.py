4bfcbe4149bc1384c175af753e92eb4ea67ab3df
chisoftltd <chisoftltd@gmail.com>
2021-03-02 14:05:11 +0000

    NumPy Array Shape

--- PythonNumPy.py
+++ PythonNumPy.py
@@ -130,3 +130,14 @@ print(x.base)
 print()
 print(y.base)
 print()
+
+# NumPy Array Shape
+# Get the Shape of an Array
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+print(arr.shape)
+print()
+
+# Create an array with 5 dimensions using ndmin using a vector with values 1,2,3,4 and verify that last dimension has value 4
+arr = np.array([21, 23, 34, 45, 4], ndmin = 5)
+print(arr)
+print('shape of array :', arr.shape)
