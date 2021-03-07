c6de879917306fd4304139de91d29e626da6b2dc
chisoftltd <chisoftltd@gmail.com>
2021-03-07 14:53:20 +0100

    NumPy Logs

--- PythonNumPy.py
+++ PythonNumPy.py
@@ -736,3 +736,26 @@ print()
 arr = np.ceil([-4.14567, 2.9876])
 print(arr)
 print()
+
+# NumPy Logs
+# Log at Base 2
+arr = np.arange(1, 10)
+print(np.log2(arr))
+print()
+
+# Log at Base 10
+arr = np.arange(1, 10)
+print(np.log10(arr))
+print()
+
+# Natural Log, or Log at Base e
+arr = np.arange(1, 10)
+print(np.log(arr))
+print()
+
+# Log at Any Base
+from math import log
+
+nplog = np.frompyfunc(log, 2, 1)
+print(nplog(100, 15))
+print()
